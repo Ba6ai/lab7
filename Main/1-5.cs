cde03fe2d7b8b95ea9cd2ff8d3ff64c02fee8c29
Ba6ai <153833234+Ba6ai@users.noreply.github.com>
2026-04-19 21:42:51 +0500

    Initial commit

---
+++ Main/1-5.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+
+namespace ex1
+{
+    internal class NewMethod
+    {
+        public static void GenWrite(string path1, string path2, int count)
+        {
+            int x = 0;
+            Random rnd = new Random();
+
+            StreamWriter writeFile1;
+            StreamWriter writeFile2;
+            try
+            {
+                writeFile1 = new StreamWriter(path1);
+                writeFile2 = new StreamWriter(path2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            while (x != count)
+            {
+                writeFile1.WriteLine(rnd.Next(0, 100));
+                writeFile2.Write(rnd.Next(0, 100) + " ");
+                x++;
+            }
+            writeFile1.Close();
+            writeFile2.Close();
+        }
+
+        public static int GetDiff(string path1)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            string s;
+
+            StreamReader readFile;
+            try
+            {
+                readFile = new StreamReader(path1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+            while ((s = readFile.ReadLine()) != null)
+            {
+                int current = int.Parse(s);
+                if( current < min ) min = current;
+                if( current > max ) max = current;
+            }
+            readFile.Close();
+            return max - min;
+        }
+
+        //public static void GetWrite2(string path2, int count)
+        //{
+        //    StreamWriter writeFile; ;
+        //    Random rnd = new Random();
+        //    int x = 0;
+
+        //    try
+        //    {
+        //        writeFile = new StreamWriter(path2);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine (e.Message);
+        //        return;
+        //    }
+        //    while (x != count)
+        //    {
+        //        writeFile.Write(rnd.Next(0, 100) + " ");
+        //        x++;
+        //    }
+        //}
+        
+        public static int GetMin(string path2)
+        {
+            int min = int.MaxValue;
+            string s;
+            StreamReader readFile;
+            
+            try
+            {
+                readFile = new StreamReader(path2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+
+            while((s = readFile.ReadLine()) != null)
+            {
+                foreach( string x in mas )
+                {
+                    string line = readFile.ReadLine();
+                    string[] mas = line.Split(' ');
+
+                    int current = int.Parse(s);
+                    if( current < min ) min = current;
+                }
+
+            }
+
+            return min;
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,23 @@
+﻿using ex1;
+using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int count = 0;
+        string path1 = "C:\\Users\\elise\\OneDrive\\УНИК\\2Курс\\2\\ЯП\\kt3\\lab7\\Main\\ex1.txt";
+        string path2 = "C:\\Users\\elise\\OneDrive\\УНИК\\2Курс\\2\\ЯП\\kt3\\lab7\\Main\\ex2.txt";
+
+        Console.Write("Введите количество элеметов: ");
+        while(!int.TryParse(Console.ReadLine(), out count))
+            Console.Write("Ошибка ввода, повторите попытку: ");
+
+        NewMethod.GenWrite(path1, path2, count);
+        Console.WriteLine("Разность макс и мин = " + NewMethod.GetDiff(path1));
+        Console.WriteLine("Минимальное число = " + NewMethod.GetMin(path2));
+
+        //NewMethod.GetWrite2(path2, count);
+
+        
+    }
+}
